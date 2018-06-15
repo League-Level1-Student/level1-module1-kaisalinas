90989c077911be7b110c97d41d0bd11d300358fa
league <league@wts14.attlocal.net>
2018-06-14 19:51:03 -0700

    HarryPotter,RaceCarRunner

---
+++ src/HarryRunner.java
@@ -0,0 +1,10 @@
+
+public class HarryRunner {
+	public static void main(String[] args) {
+		HarryPotter bob = new HarryPotter();
+		bob.makeInvisible(true);
+		bob.spyOnSnape();
+		bob.makeInvisible(false);
+		bob.castSpell("STUPEFY!");
+	}
+}
